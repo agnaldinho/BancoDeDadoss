ea279243a8a0418b65771aa20ac48d4e9ae0fa6e
agnaldinho <73916890+agnaldinho@users.noreply.github.com>
2021-05-05 22:26:53 -0300

    Alter tabla, add foreign key

--- Aula 03 - workBench e alter table.sql
+++ Aula 03 - workBench e alter table.sql
@@ -12,3 +12,22 @@ alter table Aluno modify column nomeAluno varchar(80) not null;
 
 alter table aluno change column nomeAluno nome_Aluno varchar(30);
 
+
+create table Professor
+(
+	cpf int primary key,
+    nomeProfessor varchar(50),
+    idade int
+);
+
+create table Aluno
+(
+	idAluno int primary key auto_increment,
+    nomeAluno varchar(50) not null,
+    idade int not null
+);
+
+alter table aluno add fk_professor_cpf int;
+alter table aluno 
+add foreign key(fk_professor_cpf) references professor(cpf);
+
